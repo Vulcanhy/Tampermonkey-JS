6ff06a49a5e9551b570e26ce7ba9dbff7efb60fa
Vulcanhy <vulcanhy@gmail.com>
2022-04-06 21:15:13 +0800

    Create I am Groot.js

---
+++ I am Groot.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        I am Groot!
+// @namespace   https://github.com/lzghzr/TampermonkeyJS
+// @version     0.0.4
+// @author      lzghzr
+// @description I am Groot!
+// @match       http://*/*
+// @match       https://*/*
+// @require     https://cdn.jsdelivr.net/gh/lzghzr/TampermonkeyJS@a12e5f5baeea9c13a3dc2b6cac4b6ecef29533d5/libReplaceText/libReplaceText.js
+// @license     MIT
+// @grant       none
+// @run-at      document-start
+// ==/UserScript==
+new ReplaceText([[/^[\s\S]*$/g, 'I am Groot!']], 'match');
